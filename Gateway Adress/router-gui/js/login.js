e3c4c06445fd59de915c92c1aae7a48c896f3305
sandhya <sandhyaperabathula@users.noreply.github.com>
2026-05-16 20:58:41 +0530

    update

--- Gateway Adress/router-gui/js/login.js
+++ Gateway Adress/router-gui/js/login.js
@@ -24,7 +24,7 @@ function togglePassword() {
         `;
     } else {
         passwordInput.type = "password";
-        // Update back to Eye icon
+        
         eyeIcon.innerHTML = `
             <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
             <circle cx="12" cy="12" r="3"></circle>
